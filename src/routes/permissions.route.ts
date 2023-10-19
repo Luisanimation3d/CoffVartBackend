2b6a032179827197f7c97435f0fc0db3ee8ceb64
unknown <SENA@MEDAPRCSGFSP543.FormacionSena.RED>
2023-10-19 09:37:41 -0500

    Se agregaron todos los metodos en el controlador de permissions y se añaderon las rutas de permissions

--- src/routes/permissions.route.ts
+++ src/routes/permissions.route.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { getPermissions } from "../controllers/permissions.controller";
+import { getPermissions, postPermission,  putPermission, deletePermission } from "../controllers/permissions.controller";
 
 const router = Router();
 
 router.get('/', getPermissions);
+router.post('/', postPermission);
+router.put('/:id', putPermission);
+router.delete('/:id', deletePermission);
+
 
 export default router;
