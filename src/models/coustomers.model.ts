e35466da57d68b119409a14db7de0dbf5ca7a941
unknown <SENA@MEDAPRCSGFSP537.FormacionSena.RED>
2023-10-19 10:01:15 -0500

    Models Mateo

---
+++ src/models/coustomers.model.ts
@@ -0,0 +1,34 @@
+import {DataTypes } from 'sequelize'
+import { sequelize } from '../database/config'
+
+export const coustumersModel = sequelize.define('coustumers', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name:{
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    phone: {
+        type: DataTypes.INTEGER(15),
+        allowNull: false
+
+    },
+    email: {
+        type: DataTypes.STRING(30),
+        allowNull: false
+    },
+    address: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    }, 
+    state: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+    }
+},
+{   
+    timestamps: true
+})
