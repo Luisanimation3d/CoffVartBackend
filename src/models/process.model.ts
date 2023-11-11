2f714ac3e12d9e7f49dbb75edda6dc864089b54e
AlejandroVelasHel <velalejandro23@gmail.com>
2023-11-11 15:56:39 -0500

    actualización de todo el modulo de producción

---
+++ src/models/process.model.ts
@@ -0,0 +1,25 @@
+import {DataTypes} from "sequelize";
+import {sequelize} from "../database/config";
+
+export const processModel = sequelize.define('process',{
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    state: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+    },
+},{
+    timestamps: true
+})
+
