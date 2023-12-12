fd2ac65a2d5e0ae213b6d1a69ce020a64c014c3f
MateoOsorio016 <mosorio7347@misena.edu.co>
2023-12-11 22:37:10 -0500

    Luis este es uwu

--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getOrders, getOrder, postOrder, putOrders } from "../controllers/orders.controller";
+import { getOrders, getOrder, postOrder, putOrders, deleteOrders } from "../controllers/orders.controller";
 import { PostOrdersMiddleware, GetOrdersMiddleware } from "../middlewares/orders.middlewares";
 import { extractUserMiddlewares } from "../middlewares/extractUser.middlewares";
 import {validateRoutePost} from '../middlewares/orders.middlewares';
@@ -10,5 +10,6 @@ router.get('/', /*extractUserMiddlewares, GetOrdersMiddleware,*/ getOrders);
 router.get('/:id', getOrder);
 router.post('/', /*extractUserMiddlewares, PostOrdersMiddleware, validateRoutePost,*/  postOrder);
 router.put('/:id', putOrders);
+router.delete('/:id', deleteOrders);
 
 export default router;
