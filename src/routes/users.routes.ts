ee035b3d023864f78c2a4c9ba1ecc7023b247bee
Edgar Rivera <edgardo.rivera@julius2grow.com>
2024-02-26 21:51:19 -0500

    Se colocó el upload user image

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,22 @@
 import { Router } from "express";
-import { getUsers, getUser, postUser, putUser, deleteUser, validateUserAlreadyExists } from "../controllers/users.controller";
+import multer from "multer";
+import { getUsers, getUser, postUser, putUser, deleteUser, validateUserAlreadyExists, uploadImage, getImage } from "../controllers/users.controller";
 
 // import { extractUserMiddlewares } from "../middlewares/extractUser.middlewares";
 // import { GetUsersMiddleware, PostUsersMiddleware } from "../middlewares/users.middlewares";
 
 const router = Router();
+
+const storage = multer.diskStorage({
+    destination: (req, res, cb) => {
+        cb(null, './src/uploads/users');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${file.originalname.replace(/ /g, '_')}`);
+    }
+})
+
+const upload = multer({ storage });
 
 /* The code is defining the routes for handling HTTP requests related to users. */
 // router.get('/', extractUserMiddlewares, GetUsersMiddleware, getUsers);
@@ -15,5 +27,7 @@ router.post('/', postUser);
 router.put('/:id', putUser);
 router.delete('/:id', deleteUser);
 router.get('/validate', validateUserAlreadyExists);
+router.post('/upload', upload.single('image'), uploadImage);
+router.get('/image/:image', getImage);
 
 export default router;
